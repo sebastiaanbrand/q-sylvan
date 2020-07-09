0c365659023d74f8cbec9674bb574be97e9f1f27
Sebastiaan Brand <sebastiaanbrand@gmail.com>
2020-07-09 17:18:03 +0200

    supremacy_5_1 circuit

--- test/bench_qdd.c
+++ test/bench_qdd.c
@@ -182,10 +182,80 @@ int bench_grover(int num_qubits)
     return 0;
 }
 
+uint32_t random_sqrtXY()
+{
+    return ( rand() % 2 ) ? GATEID_sqrtX : GATEID_sqrtY;
+}
+
+int bench_supremacy_5_1(uint32_t depth)
+{
+    // 5x1 "grid" from [Characterizing Quantum Supremacy in Near-Term Devices]
+
+    LACE_ME;
+    BDDVAR n_qubits = 5;
+    srand(42);
+
+    // Start with |00000> state
+    QDD qdd = qdd_create_all_zero_state(n_qubits);
+
+    // H on all qubits
+    for (BDDVAR k=0; k < n_qubits; k++) qdd = qdd_gate(qdd, GATEID_H, k);
+
+    // First 4 cycles the single qubit gates we apply are all T gates
+    if (depth > 0) {
+        qdd = qdd_cgate(qdd, GATEID_Z, 0, 1);
+        qdd = qdd_cgate(qdd, GATEID_Z, 3, 4);
+    }
+    if (depth > 1) {
+        qdd = qdd_cgate(qdd, GATEID_Z, 1, 2);
+        qdd = qdd_gate(qdd, GATEID_T, 0);
+        qdd = qdd_gate(qdd, GATEID_T, 3);
+        qdd = qdd_gate(qdd, GATEID_T, 4);
+    }
+    if (depth > 2) {
+        qdd = qdd_cgate(qdd, GATEID_Z, 2, 3);
+        qdd = qdd_gate(qdd, GATEID_T, 1);
+    }
+    if (depth > 3) {
+        qdd = qdd_cgate(qdd, GATEID_Z, 0, 1);
+        qdd = qdd_cgate(qdd, GATEID_Z, 3, 4);
+        qdd = qdd_gate(qdd, GATEID_T, 2);
+    }
+
+    // Following cycles the single qubit gates are random from {sqrt(X), sqrt(Y)}
+    for (uint32_t d = 4; d < depth; d++) {
+        switch (d % 3)
+        {
+        case 0:
+            qdd = qdd_cgate(qdd, GATEID_Z, 0, 1);    // CZ(0,1)
+            qdd = qdd_cgate(qdd, GATEID_Z, 3, 4);    // CZ(3,4)
+            qdd = qdd_gate(qdd, random_sqrtXY(), 2); // random on q2
+            break;
+        case 1:
+            qdd = qdd_cgate(qdd, GATEID_Z, 1, 2);    // CZ(1,2)
+            qdd = qdd_gate(qdd, random_sqrtXY(), 0); // random on q0
+            qdd = qdd_gate(qdd, random_sqrtXY(), 3); // random on q3
+            qdd = qdd_gate(qdd, random_sqrtXY(), 4); // random on q4
+            break;
+        case 2:
+            qdd = qdd_cgate(qdd, GATEID_Z, 2, 3);    // CZ(2,3)
+            qdd = qdd_gate(qdd, random_sqrtXY(), 0); // random on q0
+            qdd = qdd_gate(qdd, random_sqrtXY(), 1); // random on q1
+            qdd = qdd_gate(qdd, random_sqrtXY(), 4); // random on q4
+            break;
+        default:
+            break;
+        }
+    }
+
+    return 0;
+}
+
 int runbench()
 {
     //if (bench_25qubit_circuit()) return 1;
-    if (bench_grover(10)) return 1;
+    //if (bench_grover(10)) return 1;
+    if (bench_supremacy_5_1(20)) return 1;
 
     return 0;
 }
