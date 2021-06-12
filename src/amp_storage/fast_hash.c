39097b8eb745f37b9f32ce478a36397742fc17fb
Sebastiaan Brand <sebastiaanbrand@gmail.com>
2021-06-12 17:17:43 +0200

    fix compile warnings

--- src/amp_storage/fast_hash.c
+++ src/amp_storage/fast_hash.c
@@ -160,14 +160,15 @@ uint64_t MurmurHash64 ( const void * key, int len, unsigned int seed )
 
     switch(len & 7)
     {
-    case 7: h ^= ((uint64_t)data2[6]) << 48;
-    case 6: h ^= ((uint64_t)data2[5]) << 40;
-    case 5: h ^= ((uint64_t)data2[4]) << 32;
-    case 4: h ^= ((uint64_t)data2[3]) << 24;
-    case 3: h ^= ((uint64_t)data2[2]) << 16;
-    case 2: h ^= ((uint64_t)data2[1]) << 8;
+    case 7: h ^= ((uint64_t)data2[6]) << 48; break;
+    case 6: h ^= ((uint64_t)data2[5]) << 40; break;
+    case 5: h ^= ((uint64_t)data2[4]) << 32; break;
+    case 4: h ^= ((uint64_t)data2[3]) << 24; break;
+    case 3: h ^= ((uint64_t)data2[2]) << 16; break;
+    case 2: h ^= ((uint64_t)data2[1]) << 8;  break;
     case 1: h ^= ((uint64_t)data2[0]);
             h *= m;
+            break;
     };
 
     h ^= h >> r;
